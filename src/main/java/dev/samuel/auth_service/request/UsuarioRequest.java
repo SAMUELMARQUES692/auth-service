bc2e4f3f77695c89f4f0abd7dc99e8ee499f8394
samuel <samue@Samuel>
2026-08-05 15:51:09 -0300

    fix: coloca uma Anotação de @Builder na classe UsuarioRequest

--- src/main/java/dev/samuel/auth_service/request/UsuarioRequest.java
+++ src/main/java/dev/samuel/auth_service/request/UsuarioRequest.java
@@ -3,9 +3,11 @@ package dev.samuel.auth_service.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 public record UsuarioRequest(
 
         @NotBlank
