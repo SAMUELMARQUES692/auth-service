d1bb1a42c2287fec95ad07b02df483cf8b5553d2
samuel <samue@Samuel>
2026-08-12 12:49:46 -0300

    fix: Coloca o @Builder na classe UsuarioEvent

--- src/main/java/dev/samuel/auth_service/response/UsuarioEvent.java
+++ src/main/java/dev/samuel/auth_service/response/UsuarioEvent.java
@@ -1,7 +1,10 @@
 package dev.samuel.auth_service.response;
 
+import lombok.Builder;
+
 import java.io.Serializable;
 
+@Builder
 public record UsuarioEvent(
         String nome,
         String email
