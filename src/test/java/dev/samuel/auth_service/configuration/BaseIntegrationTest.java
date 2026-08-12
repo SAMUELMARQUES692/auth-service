d6cc01e6ac25ed630ff5a8310ecaad0098cd43e7
samuel <samue@Samuel>
2026-08-12 13:58:58 -0300

    feat: Cria os metodos de test da classe AuthControllerTest usando o test Containers, e cria a classe de configuração BaseIntegrationTest

---
+++ src/test/java/dev/samuel/auth_service/configuration/BaseIntegrationTest.java
@@ -0,0 +1,37 @@
+package dev.samuel.auth_service.configuration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+@Transactional
+public class BaseIntegrationTest {
+
+    @Container
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:14-alpine")
+            .withDatabaseName("auth-service")
+            .withUsername("authService")
+            .withPassword("authService");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+}
---
+++ src/test/java/dev/samuel/auth_service/controller/AuthControllerTest.java
@@ -0,0 +1,91 @@
+package dev.samuel.auth_service.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import dev.samuel.auth_service.configuration.BaseIntegrationTest;
+import dev.samuel.auth_service.entity.Scope;
+import dev.samuel.auth_service.entity.Usuario;
+import dev.samuel.auth_service.repository.ScopeRepository;
+import dev.samuel.auth_service.repository.UsuarioRepository;
+import dev.samuel.auth_service.request.AuthRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AuthControllerTest extends BaseIntegrationTest {
+
+    @Autowired
+    ScopeRepository scopeRepository;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    @Test
+    void login() throws Exception {
+        Scope scope = scopeRepository.save(
+                Scope.builder()
+                .nome("nome Test")
+                .build()
+
+        );
+
+        Usuario usuario = usuarioRepository.save(
+                Usuario.builder()
+                .nome("Nome Teste")
+                .email("Email Teste")
+                .senha(passwordEncoder.encode("Senha Teste"))
+                .scopes(List.of(scope))
+                .createdAt(LocalDateTime.now())
+                .build()
+
+        );
+
+        AuthRequest request = AuthRequest.builder()
+                .email(usuario.getEmail())
+                .senha("Senha Teste")
+                .build();
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").isNotEmpty())
+                .andExpect(jsonPath("$.tokenType").value("Bearer"))
+                .andExpect(jsonPath("$.expiresIn").value(3600));
+
+    }
+
+    @Test
+    void login_credenciaisInvalidas() throws Exception {
+        AuthRequest request = AuthRequest.builder()
+                .email("Email Teste")
+                .senha("Senha Teste")
+                .build();
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.code").value("CREDENCIAIS_INCORRETAS"));
+    }
+
+}
